5d74096669dcc5a4f3820802d97d8377ba4612f4
Daniel Jonguitud <danieljonguitud17@outlook.com>
2025-05-01 19:47:25 -0600

    updated dark mode and nvim theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,43 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -116,7 +76,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -156,13 +116,20 @@ require('lazy').setup({
     },
   },
 
+  -- {
+  --   -- Theme inspired by Atom
+  --   'navarasu/onedark.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'onedark'
+  --   end,
+  -- },
+
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    "folke/tokyonight.nvim",
+    lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
+    opts = {},
   },
 
   {
@@ -172,7 +139,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
@@ -181,7 +148,7 @@ require('lazy').setup({
           {
             'filename',
             symbols = {
-              modified = '●',      -- Text to show when the file is modified.
+              modified = '●', -- Text to show when the file is modified.
             },
           }
         },
@@ -645,7 +612,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    { name = 'nvim_lsp_signature_help'},
+    { name = 'nvim_lsp_signature_help' },
   },
 }
 
--- nvim/lua/custom/plugins/auto-dark-mode.lua
+++ nvim/lua/custom/plugins/auto-dark-mode.lua
@@ -1,14 +1,35 @@
 return {
   "f-person/auto-dark-mode.nvim",
-  config = {
-    update_interval = 1000,
-    set_dark_mode = function()
-      vim.api.nvim_set_option("background", "dark")
-      vim.cmd("colorscheme onedark")
-    end,
-    set_light_mode = function()
-      vim.api.nvim_set_option("background", "light")
-      vim.cmd("colorscheme onedark")
-    end,
-  },
+  -- Optional but recommended: Make sure lualine is loaded before this runs
+  dependencies = { 'nvim-lualine/lualine.nvim' },
+  config = function()
+    require("auto-dark-mode").setup({
+      update_interval = 500,
+      set_dark_mode = function()
+        vim.api.nvim_set_option("background", "dark")
+        vim.cmd("colorscheme tokyonight-night")
+        -- Update lualine theme
+        -- Use pcall for safety, in case lualine isn't loaded yet for some reason
+        local success, lualine = pcall(require, 'lualine')
+        if success then
+          lualine.setup({ options = { theme = 'tokyonight-night' } })
+        else
+          vim.notify("Could not update lualine theme: lualine not found.", vim.log.levels.WARN)
+        end
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option("background", "light")
+        vim.cmd("colorscheme tokyonight-day")
+        -- Update lualine theme
+        local success, lualine = pcall(require, 'lualine')
+        if success then
+          lualine.setup({ options = { theme = 'tokyonight-day' } })
+        else
+          vim.notify("Could not update lualine theme: lualine not found.", vim.log.levels.WARN)
+        end
+      end,
+    })
+    -- Optionally, trigger an initial check when the plugin loads
+    require("auto-dark-mode").init()
+  end,
 }
